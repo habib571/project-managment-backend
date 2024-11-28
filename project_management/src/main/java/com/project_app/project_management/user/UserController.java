637cc3c6b6b2e2f0035a409205e37780e549639b
HabibRouatbi <hebib@camelt-soft.com>
2024-11-28 20:24:05 +0100

    added pagination

--- project_management/src/main/java/com/project_app/project_management/user/UserController.java
+++ project_management/src/main/java/com/project_app/project_management/user/UserController.java
@@ -5,10 +5,7 @@ import com.project_app.project_management.auth.UserDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,9 +25,9 @@ public class UserController {
         return ResponseEntity.ok(new UserDTO().convertToUserDTO(currentUser));
     }
 
-    @GetMapping("/")
-    public ResponseEntity<List<User>> allUsers() {
-        List<User> users = userService.allUsers();
+    @GetMapping("/{name}")
+    public ResponseEntity<List<User>> allUsers(@PathVariable("name") String name , @RequestParam("page") int page , @RequestParam("size") int size) {
+        List<User> users = userService.allUsers(name, page, size);
         return ResponseEntity.ok(users);
     }
 
